f4302d89a9d82d3b2929f10a35b3dada6a54349c
hajunsong <hajunsong90@gmail.com>
2019-11-13 13:10:14 +0900

    axis angle method apply & vsd algorithm applying...

--- RobotArm/robotarm.h
+++ RobotArm/robotarm.h
@@ -20,10 +20,11 @@ public:
     void run_kinematics();
     void run_inverse_kinematics();
     void run_dynamics();
+    void run_inverse_kinematics_with_path_generator();
+    void run_virtual_spring_damper_algorithm();
 #endif
     void run_kinematics(double *q, double *des_pose);
     void run_inverse_kinematics(double* cur_joint, double* des_pose, double* res_joint, double* res_pose);
-    void run_inverse_kinematics_with_path_generator();
     void path_generator(double x0, double xf, double tf, double ta, std::vector<double> *path);
 
 private:
@@ -46,7 +47,7 @@ private:
         // orientation
         double Ai[9], Aijpp[9], Ai_Cij[9], Cij[9], u_vec[3];
         // position
-        double sij[3], sijp[3], ri[3], re[3], Ae[9], roll, pitch, yaw;
+        double sij[3], sijp[3], ri[3], re[3], Ae[9], re_dot[3], we[3], ori[3];
         // jacobian
         double Jvi[3], Jwi[3], re_qi[3], Ae_qi[9], r6_qi[3], A6_qi[9], Aijpp_qi[9], Cij_Aijpp[9], Ai_Cij_Aijpp_qi[9];
         double Ae_qi_31, Ae_qi_32, Ae_qi_33, Ae_qi_21, Ae_qi_11, roll_qi, pitch_qi, yaw_qi;
@@ -87,10 +88,15 @@ private:
     void inverse_kinematics(double pos_d[3], double ori_d[3]=nullptr);
         void jacobian();
     void dynamics();
+    void dynamics(double *Ta);
     void save_data();
 
     void mat(double *mat_1, double *mat_2, uint row_1, uint col_1, uint row_2, uint col_2, double *mat_3);
     void mat(double *mat_1, double *vec_2, uint row_1, uint col_1, uint row_2, double *vec_3);
+    void rpy2mat(double yaw, double pitch, double roll, double *mat);
+    void mat_to_axis_angle(double R_init[9], double R_final[9], double r[3], double *theta);
+    void axis_angle_to_mat(double r[3], double theta, double mat[9]);
+    void mat2rpy(double mat[9], double ori[3]);
 
     double Ae_31, Ae_32, Ae_33, Ae_21, Ae_11;
     double roll_q_temp1, roll_q_temp2, roll_q_temp3, roll_q_temp4;
