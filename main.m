a473239f8b9413309fa5271f6b38ef79590c2020
hajunsong <hajunsong90@gmail.com>
2019-12-12 12:24:09 +0900

    version 2 dynamic parameter update

--- main.m
+++ main.m
@@ -2,6 +2,6 @@ clc; clear all; close all;
 
 % run_kinematics;
 % run_inverse_kinematics;
-% run_dynamics;
-run_inverse_kinematics_with_path_generator;
+run_dynamics;
+% run_inverse_kinematics_with_path_generator;
 % run_virtual_spring_damper_algorithm;
